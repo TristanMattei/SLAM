827dfa430169f8e9c6f2d81eb3a867862167bb6d
Zeenshok <43819224+Zeenshok@users.noreply.github.com>
2018-10-17 17:30:33 +0200

    Add files via upload

---
+++ Rendu/D. Damien/Site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8" />
+ <title>Mon site</title>
+ </head>
+ <body>
+
+  <div id="corps">
+  <h1>Evaluation</h1>
+  <form name="site" method="get" action="connexion.php">
+  <p>
+    Identifiant : <input type="text" name="login" id="login" value=""/>
+    Mot de passe : <input type="password" name="motdepasse" id="password" value =""/>
+    <input type="submit" name="submit" value="Identification" />
+  </p>
+</form>
+</form>
+</body>
+</html>
