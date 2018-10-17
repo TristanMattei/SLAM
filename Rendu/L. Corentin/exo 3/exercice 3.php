e997b7f6827141e13cb3845102e39bf5a914234e
corentinh0 <43983743+corentinh0@users.noreply.github.com>
2018-10-17 17:29:15 +0200

    Add files via upload

---
+++ Rendu/L. Corentin/exo 3/exercice 3.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+<head>
+  <title>Page 2</title>
+</head>
+<meta charset="utf-8">
+<link rel = "stylesheet" href = "exercice 3.css"/>
+
+<p>Bienvenue<?php echo "$_POST['login']";?></p>
+<input type = "radio" name = "Addition" value = "Addition" id = "Addition"/>
+
+</html>
