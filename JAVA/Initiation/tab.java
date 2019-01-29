b2d7bbc506c054c37207fb8c7a80166291221db6
MATTEI <MATTEI@lprs.local>
2019-01-29 10:13:50 +0100

    Initiation JAVA

---
+++ JAVA/Initiation/tab.java
@@ -0,0 +1,17 @@
+
+public class tab {
+
+	public static void main(String[] args) {
+		int premiersNombres[][] = { {0,2,4,6,8},{1,3,5,7,9} };
+		 
+		for(int i = 0; i < 2; i++)
+		{    
+		  for(int j = 0; j < 5; j++)
+		  {
+		    System.out.print(premiersNombres[i][j]);       
+		  }
+		  System.out.println("");     
+		}
+	}
+
+}
